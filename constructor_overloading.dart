9d53ea8246e756a03529f7621ea03259306a378f
HoduGwaja <honeybutter9250@gmail.com>
2021-04-10 23:43:28 +0900

    [commit | 2021.04.10]

---
+++ constructor_overloading.dart
@@ -0,0 +1,26 @@
+  void main() {
+  Kiwoom LJH = new Kiwoom('이정후', 3);
+  Kiwoom PBH = new Kiwoom('박병호', 4);
+    
+    
+}
+
+class Kiwoom {
+  final name;
+  final number;
+
+  Kiwoom(
+    String name,
+    int number,
+  )   : this.name = name,
+        this.number = number;
+
+  Kiwoom.fromMap(
+    Map values,
+  )   : this.name = values['name'],
+        this.number = values['number'];
+
+  void sayName() {
+    print('키움의 ${number}번타자는 ${name} 선수입니다');
+  }
+}
